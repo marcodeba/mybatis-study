846e18599426c7760fb033643d718b710cb220a9
panning255 <panning255@jk.cn>
2020-11-04 19:12:49 +0800

    commit

--- src/main/java/com/mybatis/demo/mybatisstudy/bean/MybatisFactoryBean.java
+++ src/main/java/com/mybatis/demo/mybatisstudy/bean/MybatisFactoryBean.java
@@ -1,18 +1,20 @@
 package com.mybatis.demo.mybatisstudy.bean;
 
-import com.mybatis.demo.mybatisstudy.mapper.MybatisMapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.beans.factory.FactoryBean;
-import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Proxy;
 
-@Component
 public class MybatisFactoryBean implements FactoryBean {
+    private Class mapperInterface;
+
+    public MybatisFactoryBean(Class mapperInterface) {
+        this.mapperInterface = mapperInterface;
+    }
+
     @Override
     public Object getObject() {
-        //Tools.generateSourceCode(MybatisFactoryBean.class);
-        return Proxy.newProxyInstance(MybatisFactoryBean.class.getClassLoader(), new Class[]{MybatisMapper.class}, (proxy, method, args) -> {
+        return Proxy.newProxyInstance(MybatisFactoryBean.class.getClassLoader(), new Class[]{mapperInterface}, (proxy, method, args) -> {
             System.out.println(method.getName());
             Select annotation = method.getAnnotation(Select.class);
             System.out.println(annotation.value()[0]);
@@ -22,7 +24,7 @@ public class MybatisFactoryBean implements FactoryBean {
 
     @Override
     public Class<?> getObjectType() {
-        return MybatisMapper.class;
+        return mapperInterface;
     }
 
     @Override
--- src/test/java/com/mybatis/demo/mybatisstudy/MybatisStudyApplicationTests.java
+++ src/test/java/com/mybatis/demo/mybatisstudy/MybatisStudyApplicationTests.java
@@ -1,33 +1,30 @@
 package com.mybatis.demo.mybatisstudy;
 
-import com.mybatis.demo.mybatisstudy.factory.UserMapperFactory;
+import com.mybatis.demo.mybatisstudy.bean.MyBatisBeanDefinitionRegistar;
 import com.mybatis.demo.mybatisstudy.mapper.UserMapper;
 import com.mybatis.demo.mybatisstudy.service.MybatisService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 
 @SpringBootTest
+@Import(MyBatisBeanDefinitionRegistar.class)
 class MybatisStudyApplicationTests {
-    @Autowired
+    @Resource
     private MybatisService mybatisService;
+    @Resource
     private UserMapper userMapper;
 
-    @PostConstruct
-    void init() {
-        userMapper = (UserMapper) UserMapperFactory.getInstance(UserMapper.class);
-    }
+//    @PostConstruct
+//    void init() {
+//        userMapper = (UserMapper) UserMapperFactory.getInstance(UserMapper.class);
+//    }
 
     @Test
     void testMybatis() {
         mybatisService.query();
-        userMapper.queryUsers();
-    }
-
-    @Test
-    void testUserMapper() {
         userMapper.queryUsers();
     }
 }
