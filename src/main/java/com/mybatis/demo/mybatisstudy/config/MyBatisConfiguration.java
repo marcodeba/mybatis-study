5665645be5a4bc369ba7ef71f27ee393c1dcd054
panning255 <panning255@jk.cn>
2020-11-05 00:19:15 +0800

    commit

---
+++ src/main/java/com/mybatis/demo/mybatisstudy/config/MyBatisConfiguration.java
@@ -0,0 +1,28 @@
+package com.mybatis.demo.mybatisstudy.config;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MyBatisConfiguration {
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory() {
+        SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+        sessionFactoryBean.setDataSource(datasource());
+        return sessionFactoryBean;
+    }
+
+    @Bean
+    public DataSource datasource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+        return dataSource;
+    }
+}
